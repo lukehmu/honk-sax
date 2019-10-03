cb0a36c963f7f08cfb4b7bcc42be147af793f1a8
Luke <luke@electricputty.co.uk>
2019-10-03 14:00:09 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="gooseContainer" v-for="note in notes" v-bind:key="note">
+    <Goose :modulation="note"></Goose>
+    {{ note }}
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Goose from './components/Goose.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Goose
+  },
+  data() {
+    return {
+      notes: ['-36','-24','-12','0','12','24','36']
+    }
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Goose.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="goose">
+        <button @click="playGooseSound(modulation)">
+            I am a picture of a goose
+        </button>
+    </div>
+</template>
+<script>
+import Tone from 'tone'
+
+
+export default {
+    data() {
+        return {
+            sound: 'hello'
+        }
+    },
+    methods: {
+        playGooseSound() {
+            const gooseSoundPlayer = new Tone.Player("https://res.cloudinary.com/jlengstorf/video/upload/q_auto/v1569957993/honk-sound.mp3").sync().start(0)
+            const pitchShift = new Tone.PitchShift({
+                pitch: this.modulation
+            }).toMaster();
+            // gooseSoundPlayer.connect(pitchShift)
+            console.log(this.modulation) // eslint-disable-line
+            Tone.Buffer.on('load', () => {
+                    console.log('Ready for play') // eslint-disable-line
+            });
+            Tone.Transport.start()
+            // Tone.Transport.stop()
+            // Tone.Transport.start()
+        }
+    },
+    props: {
+        modulation: {
+            type: String,
+            default: ''
+        }
+    },
+    mounted() {
+        // const gooseSoundPlayer = new Tone.Player("https://res.cloudinary.com/jlengstorf/video/upload/q_auto/v1569957993/honk-sound.mp3").sync().start(0);
+        // const pitchShift = new Tone.PitchShift({
+        //     pitch: this.modulation
+        // }).toMaster();
+        // gooseSoundPlayer.connect(pitchShift)
+        // console.log(this.modulation)
+        // Tone.Buffer.on('load', () => {
+        //     console.log('Ready for play') // eslint-disable-line
+        // });
+    }
+}
+</script>
