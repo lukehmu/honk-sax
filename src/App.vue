76be73e99588706259d57840009f407b5e3a03e0
Luke <lukehmu@gmail.com>
2019-10-03 22:07:06 +0100

    tidy up notes object

--- src/App.vue
+++ src/App.vue
@@ -24,34 +24,13 @@ export default {
   data() {
     return {
       notes: {
-        B2: {
-          note: 'B2',
-          keyboardKey: 'a',
-        },
-        C3: {
-          note: 'C3',
-          keyboardKey: 's',
-        },
-        D3: {
-          note: 'D3',
-          keyboardKey: 'd',
-        },
-        E3: {
-          note: 'E3',
-          keyboardKey: 'f',
-        },
-        F3: {
-          note: 'F3',
-          keyboardKey: 'g',
-        },
-        G3: {
-          note: 'G3',
-          keyboardKey: 'h',
-        },
-        A3: {
-          note: 'A3',
-          keyboardKey: 'j',
-        },
+        B2: { note: 'B2', keyboardKey: 'a' },
+        C3: { note: 'C3', keyboardKey: 's' },
+        D3: { note: 'D3', keyboardKey: 'd' },
+        E3: { note: 'E3', keyboardKey: 'f' },
+        F3: { note: 'F3', keyboardKey: 'g' },
+        G3: { note: 'G3', keyboardKey: 'h' },
+        A3: { note: 'A3', keyboardKey: 'j' },
       },
     }
   },
@@ -63,7 +42,7 @@ export default {
 
 @media screen and (min-width: 900px) {
   #gooseContainer {
-  display: flex;
-}
+    display: flex;
+  }
 }
 </style>
